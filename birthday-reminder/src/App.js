ee61bbf797f8861bc39aca4d854c34bb44f992e7
Okiemute Gold <goldokiemute@gmail.com>
2022-12-19 01:12:13 +0100

    project setup, added data.js, List component, imported useState, finished list rendering basic project done

---
+++ birthday-reminder/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+// import './App.css';
+import List from "./List";
+import data from "./data";
+
+function App() {
+    const [people, setPeople] = useState(data);
+
+    return (
+        <main>
+            <section className="container">
+                <h3>{people.length} birthdays today</h3>
+
+                <div>
+                    <List people={people} />
+                </div>
+
+                <button onClick={() => setPeople([])}>Clear All</button>
+            </section>
+        </main>
+    );
+}
+
+export default App;
