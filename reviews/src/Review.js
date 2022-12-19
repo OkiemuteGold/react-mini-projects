5f700ee3e7ea263d406883b3e37d8a3837ca003d
Okiemute Gold <goldokiemute@gmail.com>
2022-12-19 21:32:07 +0100

    installed react-icons library component, added Review component and others to App.js, finished reviews component, added nextPerson and previousPerson function to set index, used checkIndex to prevent error

--- reviews/src/Review.js
+++ reviews/src/Review.js
@@ -1,9 +1,57 @@
-import { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import { useState } from "react";
+import reviews from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+    let [index, setIndex] = useState(0);
+    const { name, job, image, text } = reviews[index];
+
+    const checkIndex = () => {
+        if (index < 0) {
+            setIndex(reviews.length - 1);
+        }
+
+        if (index > reviews.length - 1) {
+            setIndex(0);
+        }
+        // console.log(index);
+    }
+
+    const previousPerson = () => {
+        setIndex(index -= 1);
+        checkIndex();
+    }
+
+    const nextPerson = () => {
+        setIndex(index += 1);
+        checkIndex();
+    }
+
+    return (
+        <article className="review">
+            <div className="img-container">
+                <img src={image} alt={name} className="person-img" />
+                <span className="quote-icon">
+                    <FaQuoteRight />
+                </span>
+            </div>
+
+            <h4 className="author">{name}</h4>
+            <p className="job">{job}</p>
+            <p className="info">{text}</p>
+
+            <div className="button-container">
+                <button className="prev-btn" onClick={previousPerson} >
+                    <FaChevronLeft />
+                </button>
+                <button className="next-btn" onClick={nextPerson} >
+                    <FaChevronRight />
+                </button>
+            </div>
+
+            <button className="random-btn" >Surprise me</button>
+        </article>
+    );
 };
 
 export default Review;
