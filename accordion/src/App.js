c23b447869b6f8a50fa9fa3d36919550cabb1296
Okiemute Gold <goldokiemute@gmail.com>
2022-12-22 01:40:35 +0100

    set SingleQuestion component, set functionality to toggle question info, and icons, updated css active header

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,9 +1,23 @@
 import React, { useState } from 'react';
-// import data from './data';
-// import SingleQuestion from './Question';
+import data from './data';
+import SingleQuestion from './Question';
 
 function App() {
-    return <h2>accordion project setup</h2>;
+    const [questions] = useState(data);
+
+    return (
+        <main>
+            <div className="container">
+                <h3>Questions and answers about login</h3>
+
+                <section className="info">
+                    {questions.map(question => {
+                        return <SingleQuestion {...question} key={question.id} />
+                    })}
+                </section>
+            </div>
+        </main>
+    )
 }
 
 export default App;
--- accordion/src/Question.js
+++ accordion/src/Question.js
@@ -1,8 +1,28 @@
-// import { useState } from 'react';
-// import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+import { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ ...question }) => {
+    const [showInfo, setShowInfo] = useState(false)
+
+    const { title, info } = question;
+
+    return (
+        <article className="question">
+            <header className={showInfo ? "active" : ""} onClick={() => setShowInfo(!showInfo)}>
+                <h4>{title}</h4>
+
+                <button className="btn" onClick={() => setShowInfo(!showInfo)} >
+                    {
+                        showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />
+                    }
+                </button>
+            </header>
+
+            {
+                showInfo && <p>{info}</p>
+            }
+        </article>
+    )
 };
 
 export default Question;
