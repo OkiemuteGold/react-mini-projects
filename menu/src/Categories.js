256232e5f72dec907d71da476408a3b1859b1a29
Okiemute Gold <goldokiemute@gmail.com>
2022-12-23 03:07:55 +0100

    added active class to button clicked, modified index.css, Categories component, updated readMe

--- menu/src/Categories.js
+++ menu/src/Categories.js
@@ -1,12 +1,21 @@
+import { useState } from "react"
+
 const Categories = ({ allCategories, filterItems }) => {
+    const [active, setActive] = useState("all");
+
+    const showCategory = (category) => {
+        setActive(category);
+        filterItems(category);
+    }
+
     return (
         <div className="btn-container">
             {allCategories.map((category, index) => (
                 <button
                     type="button"
-                    className="filter-btn"
+                    className={`filter-btn ${active === category ? "active" : ""}`}
                     key={index}
-                    onClick={() => filterItems(category)}
+                    onClick={() => showCategory(category)}
                 >
                     {category}
                 </button>
