27b116d238a80c6940c95d87ec085e2cd9b12dca
Okiemute Gold <goldokiemute@gmail.com>
2022-12-26 00:49:06 +0100

    changed from defaultValue to value-with-onChange event, used submitForm to handle conditions, and setTexts, displayed sliced texts

--- lorem-ipsum/src/App.js
+++ lorem-ipsum/src/App.js
@@ -1,22 +1,52 @@
 import React, { useState } from "react";
-// import data from "./data";
-// import List from "./List";
+import data from "./data";
 
 function App() {
-    // const [count, setCount] = useState(0);
-    // const [text, setText] = useState([]);
+    const [count, setCount] = useState(0);
+    const [texts, setTexts] = useState([]);
 
     const submitForm = (e) => {
         e.preventDefault();
-        console.log("form submitted: ", e);
+
+        // use onChange when using value attr in input, otherwise use defaultValue for mutability.
+
+        // let amount = parseInt(e.target[0].value);
+        let amount = parseInt(count);
+
+        if (amount <= 0) {
+            amount = 1;
+        }
+
+        if (amount > data.length) {
+            amount = data.length;
+        }
+
+        const allText = data.slice(0, amount);
+        // console.log(allText);
+        setTexts(allText);
     }
+
+    // here useEffect can be used like a 2-Way Dynamic data-binding: get count onChange, check condition and getText(amount)
 
     return (
         <section className="section-center">
             <h3>Tired of boring lorem ipsum?</h3>
-            <form className="lorem-form" onSubmit={submitForm}>
 
+            <form className="lorem-form" onSubmit={submitForm}>
+                <label htmlFor="amount">Paragraphs:</label>
+
+                <input type="number" name="amount" id="amount" value={count} onChange={(e) => setCount(e.target.value)} />
+
+                <button type="submit" className="btn">Generate</button>
             </form>
+
+            <article>
+                {
+                    texts.map((text, index) => {
+                        return <p key={index} >{text}</p>
+                    })
+                }
+            </article>
         </section>
     );
 }
