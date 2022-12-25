1c17ceca82cc98f91b6fca45e163cd925680a0ec
Okiemute Gold <goldokiemute@gmail.com>
2022-12-25 21:15:22 +0100

    set nextSlide as default class to position all reviews

--- slider/src/Review.js
+++ slider/src/Review.js
@@ -8,8 +8,10 @@ const Review = ({ people }) => {
                 people.map((person, index) => {
                     const { id, image, name, title, quote } = person;
 
+                    let position = "nextSlide"
+
                     return (
-                        <article key={id} >
+                        <article key={id} className={position} >
                             <img src={image} alt={name} className="person-img" />
 
                             <h4>{name}</h4>
