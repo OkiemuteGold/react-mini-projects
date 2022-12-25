e9f2c75c8c9e80abdd7a70f307cb64920246af64
Okiemute Gold <goldokiemute@gmail.com>
2022-12-25 21:05:33 +0100

    added Review component

--- slider/src/App.js
+++ slider/src/App.js
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from 'react';
-// import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-// import { FaQuoteRight } from 'react-icons/fa';
-// import data from './data';
+import data from './data';
+import Review from './Review';
 
 function App() {
-    return <h2>slider project setup</h2>;
+    const [people, setPeople] = useState(data)
+    const [index, setIndex] = useState(0)
+
+    // const setCurrentReview = () => {
+    //     const review = people[index]
+    //     setPeople()
+    // }
+
+    // useEffect(() => {
+    //     setCurrentReview();
+    // }, []);
+
+    return (
+        <section className="section">
+            <div className="title">
+                <h2>
+                    <span>/</span>reviews
+                </h2>
+            </div>
+
+            <Review people={people} />
+        </section>
+    )
 }
 
 export default App;
---
+++ slider/src/Review.js
@@ -0,0 +1,36 @@
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+
+const Review = ({ people }) => {
+    return (
+        <div className="section-center">
+            {
+                people.map((person, index) => {
+                    const { id, image, name, title, quote } = person;
+
+                    return (
+                        <article key={id} >
+                            <img src={image} alt={name} className="person-img" />
+
+                            <h4>{name}</h4>
+                            <p className="title">{title}</p>
+                            <p className="text">{quote}</p>
+
+                            <FaQuoteRight className="icon" />
+                        </article>
+                    );
+                })
+            }
+
+            <button className="prev">
+                <FiChevronLeft />
+            </button>
+
+            <button className="next">
+                <FiChevronRight />
+            </button>
+        </div>
+    )
+}
+
+export default Review;
