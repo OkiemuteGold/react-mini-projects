c0c39b25ffc5f2c8199f81b9d9cf91cc04e63749
Okiemute Gold <goldokiemute@gmail.com>
2022-12-27 00:30:30 +0100

    added SingleColor component, handle submit with submitColor function, set errorMessage on error

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -1,10 +1,72 @@
-import React, { useState } from 'react'
-// import SingleColor from './SingleColor'
-
-// import Values from 'values.js'
+import React, { useState } from "react";
+import Values from 'values.js'
+import SingleColor from "./SingleColor";
 
 function App() {
-    return <h2>color generator project</h2>
+    const [color, setColor] = useState("");
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [colorList, setColorList] = useState([]);
+
+    const submitColor = (e) => {
+        e.preventDefault();
+
+        // let letters = color.split("");
+        // for (let i = 0; i < letters.length; i++) {
+        //     if (letters[0] !== "#") {
+        //         letters.unshift("#");
+        //     }
+        // }
+        // let newColor = letters.join("");
+        // let colors = new Values(newColor).all(10);
+
+        try {
+            let colors = new Values(color).all(10);
+            console.log(colors);
+
+            setError(false);
+            setColorList(colors)
+        } catch (error) {
+            setError(true);
+            setErrorMessage(error.message);
+        }
+    };
+
+    return (
+        <>
+            <section className="container">
+                <h3>Color generator</h3>
+
+                <form onSubmit={submitColor}>
+                    <input
+                        type="text"
+                        value={color}
+                        onChange={(e) => setColor(e.target.value)}
+                        placeholder="#f15025"
+                        className={`${error ? 'error' : null}`}
+                    />
+
+                    <button className="btn" type="submit">
+                        Submit
+                    </button>
+                </form>
+
+                <p>
+                    {error && errorMessage}
+                </p>
+            </section>
+
+            <section className="colors">
+                {
+                    colorList.map((color, index) => {
+                        console.log(color);
+
+                        return <SingleColor color={color} key={index} />
+                    })
+                }
+            </section>
+        </>
+    );
 }
 
-export default App
+export default App;
