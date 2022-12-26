073aa4fdf260fa2cc876c5434d6240ea457b3cca
Okiemute Gold <goldokiemute@gmail.com>
2022-12-26 01:14:50 +0100

    added color-generator project

---
+++ color-generator/src/SingleColor.js
@@ -0,0 +1,8 @@
+// import { useState, useEffect } from 'react'
+// import rgbToHex from './utilities'
+
+const SingleColor = () => {
+    return <h4>single color</h4>
+}
+
+export default SingleColor
