2431d73383f123c3c176ac079e6e054932067ac8
Okiemute Gold <goldokiemute@gmail.com>
2023-01-30 14:46:27 +0100

    added sidebar-modal project, and components - Sidebar, Modal, Home, updated App.js

---
+++ sidebar-modal/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './Home'
+import Modal from './Modal'
+import Sidebar from './Sidebar'
+
+function App() {
+    return (
+        <>
+            <Home />
+            <Modal />
+            <Sidebar />
+        </>
+    );
+}
+
+export default App;
