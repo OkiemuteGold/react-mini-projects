8916adc4ea351e8342be4111e005e2edf9411b4b
Okiemute Gold <goldokiemute@gmail.com>
2023-02-04 00:54:46 +0100

    added stripe-submenu project, completed

---
+++ stripe-submenu/src/context.js
@@ -0,0 +1,54 @@
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+    const [page, setPage] = useState({ page: '', links: [] });
+    const [location, setLocation] = useState({});
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    };
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    };
+
+    const openSubmenu = (text, coordinates) => {
+        const page = sublinks.find((link) => link.page === text);
+        setPage(page);
+        setLocation(coordinates);
+        setIsSubmenuOpen(true);
+    };
+
+    const closeSubmenu = () => {
+        setIsSubmenuOpen(false);
+    };
+
+    const contextValues = {
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+        page,
+        location,
+    };
+
+    return (
+        <AppContext.Provider value={contextValues}>
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+// my custom hook
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
