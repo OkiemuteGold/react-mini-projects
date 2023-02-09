0b4cee643e46812756706d7fed229621bf4b7e17
Okiemute Gold <goldokiemute@gmail.com>
2023-02-09 14:37:40 +0100

    added cocktails array to CocktailList and used data in Cocktail component

--- cocktail/src/components/CocktailList.js
+++ cocktail/src/components/CocktailList.js
@@ -26,16 +26,18 @@ const CocktailList = () => {
             </h2>
         )
     }
-    //   return (
-    //     <section className='section'>
-    //       <h2 className='section-title'>cocktails</h2>
-    //       <div className='cocktails-center'>
-    //         {cocktails.map((item) => {
-    //           return <Cocktail key={item.id} {...item} />
-    //         })}
-    //       </div>
-    //     </section>
-    //   )
+
+    return (
+        <section className='section'>
+            <h2 className='section-title'>cocktails</h2>
+
+            <div className='cocktails-center'>
+                {cocktails.map((item) => {
+                    return <Cocktail key={item.id} {...item} />
+                })}
+            </div>
+        </section>
+    )
 }
 
 export default CocktailList
